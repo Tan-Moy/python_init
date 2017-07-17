a4e196f8750ec171080ca004a374647454a57f52
Tan Moy <Tan Moy>
2017-07-17 10:18:42 +0530

    getting started with python

---
+++ py.py
@@ -0,0 +1,8 @@
+"""This module is just for practise"""
+#pylint: disable=c0103
+# from __future__ import division
+
+greeting = input('User input: ')
+print(greeting)
+
+print(greeting.replace("i", "o"))
