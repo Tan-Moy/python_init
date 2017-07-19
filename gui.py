59e9761ebac69f84daa66d59c75274d734a4a1b9
Tan Moy <Tan Moy>
2017-07-19 11:24:37 +0530

    added a label and button to gui

--- gui.py
+++
@@ -1,8 +0,0 @@
-"""Building gui app v 0.1"""
-
-#pylint: disable=c0103
-
-import tkinter as tk
-win = tk.Tk()
-win.title("Python GUI")
-win.mainloop()
--- py.py
+++
@@ -1,8 +0,0 @@
-"""This module is just for practise"""
-#pylint: disable=c0103
-# from __future__ import division
-
-greeting = input('User input: ')
-print(greeting)
-
-print(greeting.replace("i", "o"))
